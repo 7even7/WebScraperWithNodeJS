325afa762dfc0290c410006fcbf1590a7c9be4be
7even7 <patrick.stahl@gmail.com>
2016-05-26 16:47:31 +0300

    Initial Commit

---
+++ Scrape.js
@@ -0,0 +1,55 @@
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var url = 'http://www.nettiauto.com/mercedes-benz/cla?id_vehicle_type=1&id_car_type=4'
+
+var fetchHTMLfrom = function(url){
+    request(url, parseCallbackedHTML);
+}
+
+
+var parseCallbackedHTML = function (error, response, html) {
+  if (error) {
+    console.log("Following error occurred "+ error);
+  }else{
+    var $ = cheerio.load(html);
+    var list = $('#listingData')
+    
+    console.log(list);
+  }
+} 
+
+fetchHTMLfrom(url);
+
+  /*
+  Linkkejä
+  Ohje:
+  https://www.smashingmagazine.com/2015/04/web-scraping-with-nodejs/
+  
+  Ohje2:
+  https://scotch.io/tutorials/scraping-the-web-with-node-js
+  
+  Scraping laajennus scheerioon:
+  http://dailyjs.com/2015/02/05/xray/
+  
+ #ToDo
+    
+    Phase 1:
+    * Haetaan lista autoja
+    * 
+    
+    
+    Tarvittavat tiedot:
+    ID
+    Valmistaja
+    Malli
+    Vuosimalli
+    Rek.Nro
+    Vetotapa
+    Vaihteisto
+    Moottoritilavuus
+    Polttoaine
+    Mittarilukema
+    Päivitetty-date
+    
+*/
