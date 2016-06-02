4657854a4a87549231b32da1b125df73ec2827a8
7even7 <patrick.stahl@gmail.com>
2016-06-02 14:32:20 +0300

    Luo Car-olioita joissa URL id + tallentaa JSONina levylle

--- Scrape.js
+++ Scrape.js
@@ -1,27 +1,63 @@
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var url = 'http://www.nettiauto.com/mercedes-benz/cla?id_vehicle_type=1&id_car_type=4'
+var jsonfile = require('jsonfile');
+var file = './JSON.json';
+var url = 'http://www.nettiauto.com/mercedes-benz/cla?id_vehicle_type=1&id_car_type=4';
+var cars = [];
 
-var fetchHTMLfrom = function(url){
-    request(url, parseCallbackedHTML);
+function Car(URL){
+  this.URL=URL;
+  this.ID=URL;
+  this.make=null;
+  this.model=null;
+  this.buildYear=null;
+  this.plateNumber=null;
+  this.drive=null;
+  this.transmission=null;
+  this.engine=null;
+  this.fuelType=null;
+  this.milage=null;
+  this.checkDate=[Date.now()];
+  this.price=null;
+
+}
+
+var getListOfCars = function(url){
+    request(url, parseResponseHTML);
 }
 
 
-var parseCallbackedHTML = function (error, response, html) {
+var parseResponseHTML = function (error, response, html) {
   if (error) {
     console.log("Following error occurred "+ error);
   }else{
     var $ = cheerio.load(html);
     var list = $(".childVifUrl.tricky_link").map(function(i, obj){return obj.attribs.href}); 
-    
-    
-    
-    console.log(typeof list);
+    for (i = 0; i<list.length;i++){
+      var newCar = new Car(list[i]);
+      cars.push(newCar);
+    }
+    writeToFile(cars);
   }
 } 
 
-fetchHTMLfrom(url);
+var writeToFile = function(object){
+  jsonfile.writeFile(file,object, function(err) {
+      if(err){
+        console.log(err);
+      }else{
+        console.log("Writing File is Done");  
+      }
+  })
+
+  
+}
+
+
+
+
+getListOfCars(url);
 
   /*
   
@@ -41,9 +77,15 @@ fetchHTMLfrom(url);
   
  #ToDo
     
-    Phase 1:
-    * Haetaan lista autoja
+    * Haetaan lista urleja
+    * Luodaan urleista olioita id = URL
+    * tallennetaan lista olioita JSONina levylle
+    * haetaan listalle hinta
+    * haetaan listalle hakuaika
+    * jos ID:llä löytyy auto, lisätään auton hakuaika (Array). Joka haulla uusi entry.
+    * haetaan auton details tiedot
     * 
+     
     
     
     Tarvittavat tiedot:
